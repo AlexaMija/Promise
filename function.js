4d4f5e7bce03884f2cec826592c0bec2422b1979
Alexa Mija <aleksandragmitrovic17@gmail.com>
2020-12-08 09:59:35 +0100

    Promis, much better version

--- function.js
+++ function.js
@@ -1,34 +1,84 @@
-const promiseA = new Promise(myExecutorFunc);
-const promiseB = promiseA.then(handleFulFilled1, handleReject1);
-const promiseC = promiseA.then(handleFulFilled2, handleReject2);
+export default function.js
+startGame();
+const warrior = ["warrior1", "warrior2", "warrior3", "warrior4", "warrior5"]
 
-new Promise(function(resolve, reject) {
-  setTimeout(() => resolve(1), 1000);
-}).then(function(result){
-  alert(result);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(result * 2), 1000)
-  });
-}).then(function(result) {
-  alert(result);
-  return new Promise((resolve, reject) =>{
-    setTimeout(() => resolve(result * 2), 1000);
-  });
-}).then(function(result) {
-  alert(result);
-});
-/*
-const promise = new
-Promise((resolve, reject) => {
-const randomNumber =
-Math.random();
-setTimeout(() => {
-if(randomNumber < ) {
-resolve ("Sta resiti");
 
-} else {
-reject ("Sta nije ok");
+const arenaWithWarriors = ( numWarriors = 5) => {
+  if (typeof numWarriors !== "number" || numWarriors > 5 || numWarriors < 1) {
+    throw Error("Please entera valid number of warriors (1 - 5)")
+  }
+  function startGame(numWarriors) {
+    if (numWarriors = 5 ) {
+      return "Let's start the game! "
+    } else {
+      return false (or null) (or "Something going wrong.")
+    }
+  }
 }
-}, broj milisekundi);
-});
- */
+class Warrior = {
+  constructor(name, health = 100, rechargeTime = 1000 * health / 100,
+  damage = healt / 100 ) {
+  this.name = name;
+  this.health = health;
+  this.rechargeTime = rechargeTime;
+  this.damage = damage;
+}
+}
+async attack(warrior, fightLog) => {
+const attackFunction (warrior, fightLog)
+setTimeout(() => {
+  function randomWarriors(min, max) {
+    return Math.flor (Math.random () * (max - min + 1)) + min }
+  var ourRandomWarrior = randomWarrior(1, 5);
+  }
+  if (ourRandomWarrior.health > 0 ) {
+  //  return async attack //Ona ima rekurzivni poziv same sebe.
+    var newWarrior = ourRandomWarrior.health - 10;
+    /**Random unit, koji ima healt > 0 i njemu smakni healt za 10
+    optrilike
+    */
+  }
+}
+  attackFunction(warrior, fightLog);
+}, 300) // 300milisek do next attack for this warrior
+};
+attackFunction(warrior, figthLog);
+}
+}
+/*korak 3 U combatSimulator funkciji napraviti instance klase warrior za
+tacan broj numWarriors koje dobijes u parametrima
+*/
+const arenaWithWarriors = (numWarriors = 5) => {
+  if (typeof numWarriors !== "number" || numWarriors > 5 || numWarriors < 1) {
+    throw Error ("Please enter a valid number of warrior (1 - 5)")
+  }
+  const newWarrior = [];
+for (let i = 1; i <= numWarriors; i++) {
+  units.push(new Warrior("Ratnik" + i));
+}
+/*Posle ove iteracije u "Warrionra" imam sve instance jedinice
+ya borbu
+*/
+}
+/* Korak 4
+U combatSimulator funkciji definisati neki loop sa setInterval koji
+ce poyvati funkciju za napad sa jednom od definisanih warriora
+*/
+const arenaWithWarriors = (numWarriors = 5) => {
+  if (typeof numWarriors || numWarriors > 5 || numWarriors < 1) {
+    throw Error ("Please enter a valid number of units ( 1 - 5)")
+  }
+  const newWarriors = [];
+  for (let i = 1 i <= numWarriors; i++) {
+    units.push(newWarrior("Ratnik" + 1));
+  }
+  const combatLog = (message = "") => {
+    console.log(message);
+  };
+  units.forEach(warrior => {
+    warrior.attack(warrior, fightLog);
+  });
+}
+/*Poyvati funkciju combatSimulator na dnu index file-a
+*/
+arenaWithWarriors(3);
